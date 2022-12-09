072cdac0bc13e285d729e48d028e54d424aaaa75
DMzartex <190478@site.asty-moulin.be>
2022-12-09 16:45:26 +0100

    UpdateLogin

--- components/header.php
+++ components/header.php
@@ -1,14 +1,17 @@
 <header>
-    <div class="logo">DigitSchool</div>
-    <div class="hamburger">
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-    </div>
-    <nav class="nav-bar">
+    <nav>
+        <!--Nom-->
+        <div class="logo">DigitSchool</div>
+        <!--3 barre du menu-->
+        <input type="checkbox" id="checkbox">
+        <label for="checkbox" id="icon">
+            <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        </label>
+
+        <!--lien de navbar -->
         <ul>
             <li>
-                <a href="#">Accueil</a>
+                <a href="#" class="active">Accueil</a>
             </li>
             <li>
                 <a href="#">Informations</a>
