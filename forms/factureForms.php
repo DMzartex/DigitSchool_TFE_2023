087d09b80b11078109ac9d1a5cf4d0c2c4642061
DMzartex <190478@site.asty-moulin.be>
2022-12-17 23:51:02 +0100

    v1

--- forms/factureForms.php
+++ forms/factureForms.php
@@ -1,35 +1,43 @@
+<link rel="stylesheet" href="css/factureForms.css">
 <div class="boxForms background-yellow">
-    <form action="" method="post">
-        <div class="display-flex flex-direction-column">
-            <label for="desti">Nom du destinataire :</label>
-            <input class="margin-input width-input height-input" type="text" name="nomDesti" id="desti">
-        </div>
-        <div class="display-flex flex-direction-column">
-            <label for="adresse">Adresse du destinataire :</label>
-            <input class="margin-input width-input height-input" type="text" name="addrDesti" id="adresse">
-        </div>
-        <div class="display-flex flex-direction-column">
-            <label for="postalCode">Code postal du destinataire :</label>
-            <input class="margin-input width-input height-input" type="text" name="postalCodeDesti" id="postalCode">
-        </div>
-        <div class="display-flex flex-direction-column">
-            <label for="communication">Communication :</label>
-            <input class="margin-input width-input height-input" type="text" name="communicationFacture" id="communication">
-        </div>
-        <div class="display-flex flex-direction-column">
-            <label for="montant">Montant de la facture :</label>
-            <input class="margin-input width-input height-input" type="text" name="montantFacture" id="montant">
-        </div>
-        <?php if(empty($_POST['nomDesti'])): ?>
-        <div class="btnSend">
-            <button type="submit" name="send" id="btnSend">Envoyer</button>
-        </div>
-        <?php endif; ?>
+    <div class="">
+        <div class="">
 
-        <?php if(!empty($_POST['nomDesti'])): ?>
-        <div class="btnEdit">
-            <button type="submit" name="edit" id="btnEdit">Modifier</button>
         </div>
-        <?php endif; ?>
-    </form>
+        <div class="">
+            <form action="" method="post">
+                <div class="display-flex-column">
+                    <label for="desti">Nom du destinataire :</label>
+                    <input class="input" type="text" name="nomDesti" id="desti">
+                </div>
+                <div class="display-flex-column">
+                    <label for="adresse">Adresse du destinataire :</label>
+                    <input class="input" type="text" name="addrDesti" id="adresse">
+                </div>
+                <div class="display-flex-column">
+                    <label for="postalCode">Code postal du destinataire :</label>
+                    <input class="input" type="text" name="postalCodeDesti" id="postalCode">
+                </div>
+                <div class="display-flex-column">
+                    <label for="communication">Communication :</label>
+                    <input class="input" type="text" name="communicationFacture" id="communication">
+                </div>
+                <div class="display-flex-column">
+                    <label for="montant">Montant de la facture :</label>
+                    <input class="input" type="text" name="montantFacture" id="montant">
+                </div>
+                <?php if(empty($_POST['nomDesti'])): ?>
+                    <div class="btnSend">
+                        <button type="submit" name="send" id="btnSend">Envoyer</button>
+                    </div>
+                <?php endif; ?>
+
+                <?php if(!empty($_POST['nomDesti'])): ?>
+                    <div class="btnEdit">
+                        <button type="submit" name="edit" id="btnSend">Modifier</button>
+                    </div>
+                <?php endif; ?>
+            </form>
+        </div>
+    </div>
 </div>
--- templates/factures/newFacture.php
+++ templates/factures/newFacture.php
@@ -1,4 +1,5 @@
 <div class="width-full height-full background-red display-flex flex-direction-column justify-content-center align-items-center">
     <h1>Création d'une nouvelle facture</h1>
+    <?php require_once "model/bannerFacture.php"?>
     <?php require_once "forms/factureForms.php"?>
 </div>
