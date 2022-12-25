bac4b7f00b0eec1aeabcf8b6cf5203b14390f0a2
DMzartex <190478@site.asty-moulin.be>
2022-12-25 21:02:56 +0100

    dernière version

---
+++ components/searchBar.php
@@ -0,0 +1,17 @@
+<div class="boxSearch">
+    <form action="" method="post">
+        <div>
+            <select class="loginSelect form-select form-select-md" name="role" aria-label="Default select example">
+                <option selected value="none">Choisissez un rôle :</option>
+                <option value="student">Etudiant</option>
+                <option value="parent">Parent</option>
+            </select>
+        </div>
+        <div>
+            <input type="text" class="searchBar" name="search" placeholder="recherchez un(e) éleve :">
+        </div>
+        <div>
+            <button type="submit" id="btnSearch">valider</button>
+        </div>
+    </form>
+</div>
--- templates/factures/newFacture.php
+++ templates/factures/newFacture.php
@@ -4,11 +4,19 @@
         <img src="./images/bill-pay-4489857-3766596.webp"  alt="" class="svgPay">
     </div>
     <div class="test">
-        <div class="test3">
-            <?php require_once 'forms/factureForms.php'?>
-        </div>
         <div class="test2">
-            <h1>New Facture !</h1>
+            <div style="width:100px;">
+                <h1>Envoyer vos factures directement via à la platforme en ligne </h1>
+            </div>
+            
+            <div class="test3">
+                <div>
+                    <?php require_once 'components/searchBar.php'?>
+                </div>
+                <div>
+                    <?php require_once 'forms/factureForms.php'?>
+                </div>
+            </div>
         </div>
     </div>
 </div>
