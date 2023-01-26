95366194ba39ade6cca5c58140a17c96ce4605f5
DMzartex <190478@site.asty-moulin.be>
2023-01-27 00:30:45 +0100

    last version

--- templates/remarque/remarque.php
+++ templates/remarque/remarque.php
@@ -2,7 +2,7 @@
 <div class="rmFullContent">
     <div class="rmSearchContent">
         <div class="rmTxtContent">
-            <h1 class="txtPInter"><span class="textGreen">Rechercher un élève</span> pour obtenir la liste des remarques. <span class="textGreen">Ajouté, supprimé et moddifer</span> des remarques <span class="textGreen">à tout moments.</span></h1>
+            <h1 class="rmTxt-1 txtPInter"><span class="textGreen">Rechercher un élève</span> pour obtenir la liste des remarques. <span class="textGreen">Ajouter, supprimer et moddifer</span> des remarques <span class="textGreen">à tout moments.</span></h1>
         </div>
         <div class="rmSearchBarContent">
             <?php require_once 'components/searchBar.php'?>
@@ -10,7 +10,9 @@
     </div>
     <div class="rmResultContent">
         <div class="rmB1">
-
+                <div class="rmB1-txt">
+                        <h1 class="rmTxt-2">Remarques :</h1>
+                </div>
         </div>
         <div class="rmB2">
 
