7b8cae6b5c9caff04de8e7398727dd40d2c2a576
DMzartex <190478@site.asty-moulin.be>
2023-02-19 17:57:57 +0100

    last version

--- controller/factureController.php
+++ controller/factureController.php
@@ -2,20 +2,19 @@
 $uri = $_SERVER['REQUEST_URI'];
 
 if($uri == "/DigitSchool_TFE_2023/index.php?/templates/factures/facture"){
-    if(!empty($_POST['role'])){
+    if(isset($_POST['role'])){
         $_SESSION['roleSearchBar'] = $_POST['role'];
         $_SESSION['roleIdSearch']  = $_POST['role'].'Id';
-        if(!empty($_POST['inputSearch'])){
+        if(isset($_POST['inputSearch'])){
             $matricule = $_POST['inputSearch'];
             if (ctype_digit($matricule)) {
                 $id = $matricule;
-                $_SESSION['resultFacture'] = getFacture($conn,$id);
+                $result = getFacture($conn,$id);
+                $_SESSION['resultFacture'] = $result;
                 var_dump($_SESSION['resultFacture']);
             } else {
                 echo "L'input ne ne contient pas uniquement des chiffres";
             }
         }
     }
-
-    require_once 'templates/factures/facture.php';
 }
